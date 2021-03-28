0b1a06fc274d58fcb1a6f2573eb6c08e380dbc59
sgadkari23 <sgadkar1@centennialcollege.ca>
2021-03-28 18:07:48 -0400

    Initial Commit

---
+++ ImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Supriya G on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
